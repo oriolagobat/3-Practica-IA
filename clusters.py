1d21352b592ef49af2aaefcacc9cf0f1fdcdcf9f
FerranAD <farandomingo@gmail.com>
2022-01-04 14:23:13 +0100

    FIniquitao

--- clusters.py
+++ clusters.py
@@ -4,9 +4,12 @@ import random
 import sys
 
 def readfile(filename: str) -> Tuple[List, List, List]:
+    """
+    Parses input file.
+    """
     headers = None
-    row_names = list()
-    data = list()
+    row_names = []
+    data = []
 
     with open(filename) as file_:
         for line in file_:
@@ -22,15 +25,24 @@ def readfile(filename: str) -> Tuple[List, List, List]:
 # .........DISTANCES........
 # They are normalized between 0 and 1, where 1 means two vectors are identical
 def euclidean(v1, v2):
+    """
+    Computes normalized euclidean distance between two vectors.
+    """
     distance = sqrt(sum([(v1[i] - v2[i]) ** 2 for i in range(len(v1))]))
     return 1 / (1 + distance)
 
 
 def euclidean_squared(v1, v2):
+    """
+    Computes squared euclidean distance between two vectors.
+    """
     return euclidean(v1, v2) ** 2
 
 
 def pearson(v1, v2):
+    """
+    Computes pearson distance between two vectors.
+    """
     # Simple sums
     sum1 = sum(v1)
     sum2 = sum(v2)
@@ -47,85 +59,23 @@ def pearson(v1, v2):
     return 1 - num / den
 
 
-# ........HIERARCHICAL........
-class BiCluster:
-    def __init__(self, vec, left=None, right=None, dist=0.0, id=None):
-        self.left = left
-        self.right = right
-        self.vec = vec
-        self.id = id
-        self.distance = dist
-
-
-def hcluster(rows, distance=pearson):
-    distances = {}  # Cache of distance calculations
-    currentclustid = -1  # Non original clusters have negative id
-
-    # Clusters are initially just the rows
-    clust = [BiCluster(row, id=i) for (i, row) in enumerate(rows)]
-
-    """
-    while ...:  # Termination criterion
-        lowestpair = (0, 1)
-        closest = distance(clust[0].vec, clust[1].vec)
-
-        # loop through every pair looking for the smallest distance
-        for i in range(len(clust)):
-            for j in range(i+1, len(clust)):
-                distances[(clust[i].id, clust[j].id)] = ...
-
-            # update closest and lowestpair if needed
-            ...
-        # Calculate the average vector of the two clusters
-        mergevec = ...
-
-        # Create the new cluster
-        new_cluster = BiCluster(...)
-
-        # Update the clusters
-        currentclustid -= 1
-        del clust[lowestpair[1]]
-        del clust[lowestpair[0]]
-        clust.append(new_cluster)
-    """
-
-    return clust[0]
-
-
-def printclust(clust: BiCluster, labels=None, n=0):
-    # indent to make a hierarchy layout
-    indent = " " * n
-    if clust.id < 0:
-        # Negative means it is a branch
-        print(f"{indent}-")
-    else:
-        # Positive id means that it is a point in the dataset
-        if labels == None:
-            print(f"{indent}{clust.id}")
-        else:
-            print(f"{indent}{labels[clust.id]}")
-    # Print the right and left branches
-    if clust.left != None:
-        printclust(clust.left, labels=labels, n=n + 1)
-    if clust.right != None:
-        printclust(clust.right, labels=labels, n=n + P1)
-
-
-
 # ......... K-MEANS ..........
 class KMeans:
+    """
+    Represents k-means algorithm.
+    """
     def __init__(self, rows, distance=euclidean_squared, k=4):
         self.rows = rows
         self.distance = distance
         self.k = k
         self.clusters = None
 
-    def find_best_startconfig(self, data, iterations=10, bestresult=None):
+    def find_best_startconfig(self, iterations=10, bestresult=None):
         """
         Applies KMeans with a restarting policy and stores the best result based on
         the total distance of each item to it's closest centroid.
         """
-        for i in range(iterations):
+        for _ in range(iterations):
             # Find clusters
             initial_centroids = self.init_centroids()
             centroids, totaldistance = self.find_clusters(initial_centroids)
@@ -156,13 +106,14 @@ class KMeans:
         Applies given iterations of cluster finding.
         """
         # Update clusters on each iteration
-        for t in range(iterations):
+        for _ in range(iterations):
             #print("Iteració %i" % t)
             #print(centroids[0][0])
             distances, matches = self._find_closest_centroids(centroids)
 
             # If the results haven't changed, stop
-            if matches == lastmatches: break
+            if matches == lastmatches:
+                break
             lastmatches = matches
 
             # Update centroid positions
@@ -181,41 +132,40 @@ class KMeans:
         bestdistances = [0 for i in range(len(self.rows))]
 
         # For each row
-        for j in range(len(self.rows)):
+        for rowid, row in enumerate(self.rows):
 
             # Set centroid 0 as closest by default
-            row = self.rows[j]
             bestmatch = 0
             bestdistance = self.distance(centroids[0], row)
 
             # Find the closest centroid
-            for i in range(len(centroids)):
-                d = self.distance(centroids[i],row)
-                # print("Distancia a bestmatch: %i Distancia a K%i: %i" % (distance(clusters[bestmatch],row), i, d), file=sys.stderr)
-                if d > self.distance(centroids[bestmatch],row): 
-                    bestmatch=i
-                    bestdistance=d
-            
+            for centroid_id, centroid in enumerate(centroids):
+                distance = self.distance(centroid,row)
+                if distance > self.distance(centroids[bestmatch],row):
+                    bestmatch=centroid_id
+                    bestdistance=distance
+
             # Store results
-            bestdistances[j] = bestdistance
-            bestmatches[bestmatch].append(j)
-        
+            bestdistances[rowid] = bestdistance
+            bestmatches[bestmatch].append(rowid)
+
         return (bestdistances, bestmatches)
 
-    
+
     def _move_centroids(self, centroids, matches):
         """
         Updates each centroid's position.
         """
         # For each centroid
-        for i in range(len(centroids)):
+        for centroid_id, _ in enumerate(centroids):
+        #for i in range(len(centroids)):
             avgs = [0.0] * len(self.rows[0])
 
             # That has at least one item
-            if len(matches[i]) > 0:
+            if len(matches[centroid_id]) > 0:
 
                 # For each item it has
-                for rowid in matches[i]:
+                for rowid in matches[centroid_id]:
 
                     # Add the value of it's attributes to the subtotal
                     for attrid in range(len(self.rows[rowid])):
@@ -223,20 +173,21 @@ class KMeans:
 
                 # For each attribute get the mean value
                 for j in range(len(avgs)):
-                    avgs[j]/=len(matches[i])
+                    avgs[j]/=len(matches[centroid_id])
 
                     # Move centroid
-                    centroids[i]=avgs
-    
+                    centroids[centroid_id]=avgs
 
-def test_different_kvalues(data, begin, end, incr, restarts, totaldistances=[]):
+
+def test_different_kvalues(data, begin, end, incr, restarts):
     """
-    Given dataset and a range of k values and a restart policy value, gives 
+    Given dataset and a range of k values and a restart policy value, gives
     back the kmeans total distance for each value. Used for analisis purposes.
     """
+    totaldistances  = []
     for i in range(begin, end, incr):
         kmeans = KMeans(data, k=i)
-        _, totaldistance = kmeans.find_best_startconfig(data, iterations=restarts)
+        _, totaldistance = kmeans.find_best_startconfig(iterations=restarts)
         totaldistances.append(totaldistance)
     return totaldistances
 
@@ -247,7 +198,7 @@ def main():
         filename = sys.argv[1]
     except IndexError:
         filename = "blogdata.txt"
-    row_names, headers, data = readfile(filename)
+    _, _, data = readfile(filename)
 
     # APARTAT 1 (t9)
     kmeans = KMeans(data)
@@ -261,13 +212,15 @@ def main():
     # APARTAT 2 (t11)
     restarts = 10
     kmeans = KMeans(data)
-    centroids, totaldistance = kmeans.find_best_startconfig(data, iterations=restarts)
-    print("[Apartat 2] (%i Restarts) Distància total als centroides: %2.3f\n" % (restarts, totaldistance))
+    centroids, totaldistance = kmeans.find_best_startconfig(iterations=restarts)
+    print("[Apartat 2] (%i Restarts) Distància total als centroides: %2.3f\n"
+        % (restarts, totaldistance))
 
     # APARTAT 3 (t10)
     result = test_different_kvalues(data, 1, 5, 1, restarts)
     for i in range(len(result)):
-        print("[Apartat 3] (%i Clusters - %i Restarts) Distància total als centroides: %2.3f" % (i + 1, restarts, result[i]))
+        print("[Apartat 3] (%i Clusters - %i Restarts) Distància total als centroides: %2.3f"
+            % (i + 1, restarts, result[i]))
 
 
 if __name__ == "__main__":
